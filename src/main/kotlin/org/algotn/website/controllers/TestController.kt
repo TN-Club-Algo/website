269f0ed212fc805e9c32b3d36a36776e500504d0
ugo <ugo.gosso@gmail.com>
2023-05-31 11:20:56 +0200

    web-socket .kt 2

---
+++ src/main/kotlin/org/algotn/website/controllers/TestController.kt
@@ -0,0 +1,14 @@
+package org.algotn.website.controllers
+
+import org.algotn.website.Test
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.stereotype.Controller
+
+@Controller
+class TestController {
+    @MessageMapping("/results")
+    @SendTo("/return/tests")
+    fun sendTest(message: Test): Test { return message; }
+}
